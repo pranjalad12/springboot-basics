b7a53f03874896503ba3418bf6885539591dbee5
pranjalad12 <lci2021021@iiitl.ac.in>
2024-03-13 18:23:40 +0530

    homecontroller using modelandview

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,20 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("home")
-    // @ResponseBody
-    public String home(@RequestParam("name") String name, HttpSession session){
-        // HttpSession session=req.getSession();
-        // String name=req.getParameter("name");
-        System.out.println("this is homepage : " + name);
-        session.setAttribute("name", name);
-        return "home.jsp";
+    @RequestMapping("/home")
+    public ModelAndView home(@RequestParam("name") String name){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", name);
+        mv.setViewName("home");
+        return mv;
     }
 }
