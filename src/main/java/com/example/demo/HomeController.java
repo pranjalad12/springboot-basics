6a5402da8d833c8640f2c34c2d5c95c211aab47f
pranjalad12 <lci2021021@iiitl.ac.in>
2024-03-13 18:02:35 +0530

    using session to send data to webpage

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -12,9 +13,9 @@ public class HomeController {
 
     @RequestMapping("home")
     // @ResponseBody
-    public String home(HttpServletRequest req){
-        HttpSession session=req.getSession();
-        String name=req.getParameter("name");
+    public String home(@RequestParam("name") String name, HttpSession session){
+        // HttpSession session=req.getSession();
+        // String name=req.getParameter("name");
         System.out.println("this is homepage : " + name);
         session.setAttribute("name", name);
         return "home.jsp";
